3746f1ccba20c7dd3798ad7835068b9d0deff2d1
Maxim Tsvetkov <pushiniiist@gmail.com>
2024-05-28 08:34:19 +0300

    work with api (hope it works)

--- router.go
+++ router.go
@@ -7,19 +7,8 @@ import (
 
 var rates []rate
 
-func getAlbums(c *gin.Context) {
+func getRates(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, rates)
-}
-
-func postAlbums(c *gin.Context) {
-	var newAlbum rate
-
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	rates = append(rates, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 func getRateByName(c *gin.Context) {
@@ -33,11 +22,36 @@ func getRateByName(c *gin.Context) {
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "rate not found"})
 }
 
+func deleteRateByName(c *gin.Context) {
+	name := c.Param("name")
+	for index, rate := range rates {
+		if rate.Name == name {
+			rates = append(rates[:index], rates[index+1:]...)
+			c.IndentedJSON(http.StatusOK, rate)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "rate not found"})
+}
+
+func editRateByName(c *gin.Context) {
+	name := c.Param("name")
+	for index, rate := range rates {
+		if rate.Name == name {
+			rates = append(rates[:index], rates[index+1:]...)
+			rates = append(rates, rate)
+			c.IndentedJSON(http.StatusOK, rate)
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "rate not found"})
+}
+
 func routerInit() {
 	router := gin.Default()
-	router.GET("/", getAlbums)
-	router.GET("/:name", getRateByName)
-	router.POST("/", postAlbums)
+	router.GET("/rates", getRates)
+	router.GET("/rates/get/:name", getRateByName)
+	router.POST("/rates/delete/:name", deleteRateByName)
+	router.POST("/rates/edit/:name", editRateByName)
 
 	router.Run(":8080")
 }
